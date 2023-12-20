f006b569671812738b9fbc40655d8abc9f3512ce
Mohasalyan <mohammed.s.alyan@gmail.com>
2023-12-20 23:32:24 +0200

    neww server

--- server/server.js
+++ server/server.js
@@ -1,50 +1,51 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import { config } from 'dotenv';
-import router from './router/route.js';
+const dotenv = require("dotenv");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
+const connectToDB = require("./middlewares/dbConnect.js");
+// const indexRouter = require("./routes/index.routes.js");
+// const testRouter = require("./routes/test.routes.js");
 
-/** import connection file */
-import connect from './database/conn.js';
+const indexRouter = require("./routes/user.route.js");
+dotenv.config({ path: "./config/config.env" }); // Adjust the path as needed
 
-const app = express()
-
-
-/** app middlewares */
-app.use(morgan('tiny'));
-app.use(cors());
-app.use(express.json());
-config();
-
-
-/** appliation port */
+// Create an Express app
+const app = express();
 const port = process.env.PORT || 8080;
+// const { createAdminUserIfNotExist } = require("./utils/adminUtils.js");
 
 
-/** routes */
-app.use('/api', router) /** apis */
+
+// Connect to MongoDB
+connectToDB();
+
+// Middlewares
+app.use(express.json());
+app.use(cors({ origin: "*" })); // Allow requests from any origin
+app.use(helmet()); // Enhance security by setting various HTTP headers
+
+app.use(morgan("dev")); // Log HTTP requests
+app.use(bodyParser.json()); // Parse request bodies for JSON
+app.use(bodyParser.urlencoded({ extended: true })); // Parse request bodies for x-www-form-urlencoded
 
 
-app.get('/', (req, res) => {
-    try {
-        res.json("Get Request")
-    } catch (error) {
-        res.json(error)
-    }
-})
+// Routes
+// app.use("/api", indexRouter);
+// app.use("/", testRouter);
+app.use("/api/auth", indexRouter);
 
+app.use((err, rea, res, next) => {
+  const errorStatus = err.code || 500;
+  const errorMessage = err.message || "something went wrong!";
+  return res.status(errorStatus).json(errorMessage);
+});
+// Create admin user if not exist
+// createAdminUserIfNotExist();
 
-/** start server only when we have valid connection */
-connect().then(() => {
-    try {
-        app.listen(port, () => {
-            console.log(`Server connected to http://localhost:${port}`)
-        })
-    } catch (error) {
-        console.log("Cannot connect to the server");
-    }
-}).catch(error => {
-    console.log("Invalid Database Connection");
-})
-
+app.listen(port, () => {
+  console.log(`Server listening successfully on http://localhost:${port}`);
+});
