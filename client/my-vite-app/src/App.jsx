4ae9f65d21626ef11336c43131e7a3548ebefe47
Mahmoud Dana <mahmoudd3na@hotmail.com>
2023-12-21 01:06:39 +0200

    yes

--- client/my-vite-app/src/App.jsx
+++ client/my-vite-app/src/App.jsx
@@ -7,21 +7,42 @@ import Main from './components/Main';
 import Quiz from './components/Quiz';
 import Result from './components/Result';
 import { CheckUserExist } from './helper/helper';
+import Layout from './components/Layout';
+import Signin from './components/Signin';
+import Register from './components/Register';
+
 
 
 /** react routes */
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Main></Main>
-  },
-  {
-    path: '/quiz',
-    element: <CheckUserExist><Quiz /></CheckUserExist>
-  },
-  {
-    path: '/result',
-    element: <CheckUserExist><Result /></CheckUserExist>
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Main />
+
+      },
+      {
+        path: '/quiz',
+        element: <CheckUserExist><Quiz /></CheckUserExist>
+      },
+      {
+        path: '/result',
+        element: <CheckUserExist><Result /></CheckUserExist>
+      },
+      {
+        path: '/signin',
+        element: <Signin />
+      },
+      {
+        path: '/register',
+        element: <Register />
+      },
+
+
+    ]
   },
 ])
 
