fa15a1201bd4beb20f94aa977bf7249d40b9ef8c
Mahmoud Dana <mahmoudd3na@hotmail.com>
2023-12-20 16:01:24 +0200

    new2

--- client/my-vite-app/src/components/Quiz.jsx
+++ client/my-vite-app/src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Questions from './Questions'
 
 import { MoveNextQuestion, MovePrevQuestion } from '../hooks/FetchQuestion';
@@ -17,49 +17,49 @@ export default function Quiz() {
     const dispatch = useDispatch()
 
     /** next button event handler */
-    function onNext(){
-        if(trace < queue.length){
+    function onNext() {
+        if (trace < queue.length) {
             /** increase the trace value by one using MoveNextAction */
             dispatch(MoveNextQuestion());
 
             /** insert a new result in the array.  */
-            if(result.length <= trace){
+            if (result.length <= trace) {
                 dispatch(PushAnswer(check))
             }
         }
-     
+
         /** reset the value of the checked variable */
         setChecked(undefined)
     }
 
     /** Prev button event handler */
-    function onPrev(){
-        if(trace > 0){
+    function onPrev() {
+        if (trace > 0) {
             /** decrease the trace value by one using MovePrevQuestion */
             dispatch(MovePrevQuestion());
         }
     }
 
-    function onChecked(check){
+    function onChecked(check) {
         setChecked(check)
     }
 
     /** finished exam after the last question */
-    if(result.length && result.length >= queue.length){
+    if (result.length && result.length >= queue.length) {
         return <Navigate to={'/result'} replace={true}></Navigate>
     }
 
-  return (
-    <div className='container'>
-        <h1 className='title text-light'>Quiz Application</h1>
+    return (
+        <div className='container'>
+            <h1 className='title text-light'>Quiz Application</h1>
 
-        {/* display questions */}
-        <Questions onChecked={onChecked} />
+            {/* display questions */}
+            <Questions onChecked={onChecked} />
 
-        <div className='grid'>
-            { trace > 0 ? <button className='btn prev' onClick={onPrev}>Prev</button> : <div></div>}
-            <button className='btn next' onClick={onNext}>Next</button>
+            <div className='grid'>
+                {trace > 0 ? <button className='btn prev' onClick={onPrev}>Prev</button> : <div></div>}
+                <button className='btn next' onClick={onNext}>Next</button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
